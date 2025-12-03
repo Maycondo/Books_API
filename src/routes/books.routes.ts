4db12ed74332e2692ce191dba54b9884b40bc1d9
maycon <maycondouglasniculau@gmail.com>
2025-12-03 15:20:45 -0300

    finalizando

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -13,7 +13,7 @@ router.get("/Books", async (req: Request, res: Response) => {
 
 router.get("/:id", async (req: Request, res: Response) => {
   getBooksId(req, res);
-});
+}); 
 
 router.post("/Bookadd", async (req: Request, res: Response) => {
   postBooks(req, res);
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import router from "./routes/books.routes";
 import cors from "cors";
 
-const app = express();
-const PORT = 4000;
+const app = express();  
+const PORT = 5000;
 
 app.use(cors()); 
 app.use(express.json());
